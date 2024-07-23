03a1522739bd27635df7dfef0f4a6e4950c1adfa
Oyawe <oyawegoodness@gmail.com>
2024-07-23 15:56:02 -0700

    dark mode added

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,6 +3,7 @@ import MenuItem from "./MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
+import DarkMode from "./DarkMode";
 
 const Header = () => {
   return (
@@ -11,12 +12,15 @@ const Header = () => {
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href="/" className="flex items-center gap-1">
-        <span className="px-1 py-2 text-2xl font-bold rounded-lg bg-amber-500">
-          IMDB
-        </span>
-        <span className="hidden text-xl sm:inline">clone</span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkMode />
+        <Link href="/" className="flex items-center gap-1">
+          <span className="px-1 py-2 text-2xl font-bold rounded-lg bg-amber-500">
+            IMDB
+          </span>
+          <span className="hidden text-xl sm:inline">clone</span>
+        </Link>
+      </div>
     </div>
   );
 };
