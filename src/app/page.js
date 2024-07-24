4b2b0a3b12e66755c9d741e406c07941f22d80c2
Oyawe <oyawegoodness@gmail.com>
2024-07-23 18:02:53 -0700

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
-import React from 'react'
+const API_KEY = process.env.API_KEY;
 
-const Home = () => {
+import React from 'react'
+import Results from './components/Results';
+
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`);
+  const data = await res.json();
+  //if response is not ok
+  if (!res.ok) {
+    throw new Error('Failed to fetch')
+  }
+  const results = data.results
+  // console.log(results);
+
+
   return (
-    <div>Home</div>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
 
