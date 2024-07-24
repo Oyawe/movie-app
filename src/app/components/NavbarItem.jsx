b5647e3ade19f38c8c8685424924656658aadc47
Oyawe <oyawegoodness@gmail.com>
2024-07-24 09:24:46 -0700

    suspense for useSearchParams

--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
@@ -7,17 +7,19 @@ export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <div>
-      <Link
-        className={`hover:text-amber-600 transition ease-in duration-300 font-semibold ${
-          genre === param
-            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-            : ""
-        }`}
-        href={`/?genre=${param}`}
-      >
-        {title}
-      </Link>
-    </div>
+    <Suspense>
+      <div>
+        <Link
+          className={`hover:text-amber-600 transition ease-in duration-300 font-semibold ${
+            genre === param
+              ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+              : ""
+          }`}
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </div>
+    </Suspense>
   );
 }
